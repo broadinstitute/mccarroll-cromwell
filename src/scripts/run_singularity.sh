cce7b7995153f01ef9e9ddff718152c11937d02a
kshakir <kshakir@broadinstitute.org>
2024-06-21 14:11:48 -0400

    Updates to singularity scripts and cromwell: (#16)
    - Use sub-dirs for singularity images and logs
    - Changed run_singularity.sh -a to mount rw, with a new -r to mount ro
    - Bumped cromwell branch to a newer version

--- src/scripts/run_singularity.sh
+++ src/scripts/run_singularity.sh
@@ -36,7 +36,7 @@ current_dir=$(dirname "${BASH_SOURCE[0]}")
 # Known race condition: if two users build the same image at the same time, the
 # second user will overwrite the first user's image possibly while the first user
 # is still using it.
-get_default_cache_dir() {
+get_default_singularity_dir() {
   # Ignore errors from "groups" due to the grid engine group id.
   # https://github.com/rcgsheffield/sheffield_hpc/issues/686
   if { groups 2>/dev/null || true ; } | tr ' ' '\n' | grep -Fxq dropseqgrp; then
@@ -46,26 +46,27 @@ get_default_cache_dir() {
   fi
 }
 
-default_cache_dir=$(get_default_cache_dir)
+default_singularity_dir=$(get_default_singularity_dir)
 default_log_label_raw="$USER@$HOSTNAME"
 default_lock_timeout=$((90 * 60))
 
-bind_autofs=false
+bind_autofs=
 singularity_binds=
 singularity_args=
-cache_dir=$default_cache_dir
+singularity_dir=$default_singularity_dir
 log_label_raw=$default_log_label_raw
 lock_timeout=$default_lock_timeout
 
 usage() {
   cat >&2 <<EOF
-USAGE: $progname [-c cache_dir] [-a] [-b singularity_mount] [-s singularity_arguments] [-l log_label] [-t lock_timeout] [-h] docker_image docker_command [container_args...]
+USAGE: $progname [-c singularity_dir] [-a] [-b singularity_mount] [-s singularity_arguments] [-l log_label] [-t lock_timeout] [-h] docker_image docker_command [container_args...]
 Run a singularity container
 
--a                     : Mount all existing autofs mounts into the container.
+-a                     : Mount all existing autofs mounts into the container as read-write.
+-r                     : Mount all existing autofs mounts into the container as read-only.
 -b <singularity_mount> : Custom bind path spec in the format src[:dest[:opts]].
 -s <singularity_args>  : A space separated string of arguments to pass to singularity. Default: "".
--c <cache_dir>         : Directory to cache downloaded docker images. Default: "$default_cache_dir".
+-c <singularity_dir>   : Parent directory to download and cache docker images. Default: "$default_singularity_dir".
 -l <log_label>         : A label to use for logging. Default: "$default_log_label_raw".
 -t <lock_timeout>      : Timeout in seconds for acquiring a cache directory lock. Default: "$default_lock_timeout".
 -h                     : Show this help message.
@@ -76,19 +77,21 @@ EOF
 }
 
 make_autofs_binds() {
+  mount_option=$1
   # Do not mount /home, as users home directories often get searched for packages that are not in the container.
   awk '$3 == "autofs" {print $2}' /etc/mtab \
   | grep -v '^/proc/' \
   | grep -v '^/home$' \
-  | awk '{print "--bind "$1":"$1":ro"}'
+  | awk '{print "--bind "$1":"$1":'"$mount_option"'"}'
 }
 
-while getopts ":ab:s:c:l:t:h" options; do
+while getopts ":arb:s:c:l:t:h" options; do
   case $options in
-    a) bind_autofs=true;;
+    a) bind_autofs=rw;;
+    r) bind_autofs=ro;;
     b) singularity_binds="$singularity_binds --bind $OPTARG";;
     s) singularity_args=$OPTARG;;
-    c) cache_dir=$OPTARG;;
+    c) singularity_dir=$OPTARG;;
     l) log_label_raw=$OPTARG;;
     t) lock_timeout=$OPTARG;;
     h) usage; exit 1;;
@@ -116,12 +119,12 @@ shift 1
 # Change non alpha-numeric-ish characters to underscores
 docker_name="${docker_image//[^A-Za-z0-9._-]/_}"
 
-singularity_image="$cache_dir/$docker_name.sif"
+singularity_image="$singularity_dir/images/$docker_name.sif"
 
 # shellcheck disable=SC2046
 "$current_dir/pull_singularity.sh" \
   -o "$singularity_image" \
-  -c "$cache_dir" \
+  -c "$singularity_dir" \
   -l "$log_label_raw" \
   -t "$lock_timeout" \
   "$docker_image"
@@ -131,7 +134,7 @@ singularity_image="$cache_dir/$docker_name.sif"
 singularity exec \
    --containall \
    --cleanenv \
-   $(if [[ $bind_autofs == true ]]; then make_autofs_binds; fi) \
+   $(if [[ -n $bind_autofs ]]; then make_autofs_binds $bind_autofs; fi) \
    $singularity_binds \
    $singularity_args \
   "$singularity_image" \
